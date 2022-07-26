186eb490fdea06db2902e07c63012bba11abf573
Volkan Salma <volkansalma@yahoo.com>
2022-07-26 13:30:03 +0200

    qr2pose is added needs to be tested

--- cam_calib.py
+++ cam_calib.py
@@ -26,7 +26,7 @@ objectp3d = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
 objectp3d[0, :, :2] = np.mgrid[0:CHECKERBOARD[0],
                                0:CHECKERBOARD[1]].T.reshape(-1, 2)
 
-#scale the unit squares by 20mm
+# scale the unit squares by 20mm
 objectp3d = np.multiply(objectp3d, 0.02)
 
 prev_img_shape = None
