98d2aeecc7e1a8291c5e85fe547d6e7e959612ec
Volkan Salma <volkansalma@yahoo.com>
2022-08-02 13:14:31 +0200

    data is added to qr code, zero referced printed version on the repo

--- makeqr.py
+++ makeqr.py
@@ -1,15 +1,46 @@
 import qrcode
-import time    
+import time
 
+"""
+All systems are right handed
+x forward
+y left
+z up
+
+optical_z forward
+optical_x right
+optical_y down
+
+roll, pitch, yaw about X, Y, Z axes respectively
+"""
+
+magic_word = 'https://volkansalma.blogspot.com'
+
+# in meters
+x = 0.00000
+y = 0.00000
+z = 0.00000
+
+# in radians
+roll = 0.00000
+pitch = 0.00000
+yaw = 0.00000
 
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
+    box_size=50,
+    border=10,
 )
 
-qr.add_data('https://volkansalma.blogspot.com')
+qr.add_data(magic_word)
+qr.add_data(' x:' + str(x))
+qr.add_data(' y:' + str(y))
+qr.add_data(' z:' + str(z))
+qr.add_data(' roll:' + str(roll))
+qr.add_data(' pitch:' + str(pitch))
+qr.add_data(' yaw:' + str(yaw))
+
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white")
